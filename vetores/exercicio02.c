77797c7fdd4ecdc4b4e8d5aa928030e24fe009b6
Luana Lira <154566662+liralves@users.noreply.github.com>
2024-09-06 17:29:27 +0000

    Arualização do exercicio proposto em sala de aula.

--- vetores/exercicio02.c
+++ vetores/exercicio02.c
@@ -2,11 +2,13 @@
 #define CAPAC 50
 void fillVector(int[], int);
 void printVector(int v[], int);
+int removeElement(int[], int, int);
 int binarySearch(int[], int, int);
+void fillElement(int[], int, int, int);
 
 int main(){
 
-    int vector[CAPAC], size = 0, opt = 0, value = 0, x;
+    int vector[CAPAC], size = 0, opt = 0, value = 0, x = 0, y = 0, z = 0, position = 0;
 
     do{
         printf("Enter with size of vector (3 and %d):\n", CAPAC);
@@ -35,13 +37,28 @@ int main(){
             scanf("%d", &value);
             x = binarySearch(vector, size, value);
             if(x != -1){
-                printf("Element found in position %d.\n", x);
+                printf("Element found in position %d.\n", (x+1));
             }else {
                 printf("Element not found.\n");
             }
         }
         case 3:
-            
+            printf("Position of the element to be removed:\n");
+            scanf("%d", &value);
+            y = removeElement(vector, size, value);
+            size--;
+            printVector(vector, size);
+            break;
+        case 4:
+            printf("Value to be entered and the position:\n");
+            scanf("%d", &value);
+            scanf("%d", &position);
+            fillElement(vector, size, value, position);
+            size++;
+            break;
+        case 5:
+            break;
+
         default:
             break;
         }
@@ -68,18 +85,15 @@ int binarySearch(int v[], int size, int value){
 }
 
 void fillVector(int v[], int size){
-    char last;
-    for(int i = 0; i < size; i++){
-        do{
-        scanf("%d%c", &v[i], &last);
-        }while(last == '\0');
-        int j = i;
-        while(j > 0 && v[j] < v[j-1]){
-            int aux = v[j];
-            v[j] = v[j - 1];
-            v[j - 1] = aux;
-            j--;
+    int aux;
+    for(int i = 1; i < size + 1; i++){
+        scanf("%d", &v[i]);
+        aux = v[i];
+        int j;
+        for(j = i; (j > 0) && (aux < v[j - 1]); j--){
+            v[j] = v[j -1];
         }
+        v[j] = aux;
     }
 }
 
@@ -90,3 +104,17 @@ void printVector(int v[], int size){
 }
 
 
+int removeElement(int v[], int size, int Rvalue){
+    int x = v[Rvalue];
+    for(int j = Rvalue + 1; j < size; j++){
+        v[j - 1] = v[j];
+    }
+    return x;
+}
+
+void fillElement(int v[], int size, int value, int index){
+    for(int j = size; j > index; j--){
+        v[j] = v[j -1];
+    }
+    v[index] = value;
+}
