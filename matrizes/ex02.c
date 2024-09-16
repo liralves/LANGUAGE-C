e9665f5ff38cd6c5e6d122cd26c2ecca282f29e6
liralves <larissa.liraalves2@gmail.com>
2024-09-16 23:43:49 +0000

    Exercicio 02 da lista de matrizes basico

---
+++ matrizes/ex02.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define lin 10
+#define col 10
+void searchNumber(int[lin][col], int, int, int);
+void ordemMatrix(int*, int*);
+void fillMatrix(int[lin][col], int*, int*);
+
+int main(){
+
+    int matrix[lin][col], l = 0, c = 0, k = 0;
+
+    ordemMatrix(&l, &c);
+    fillMatrix(matrix, &l, &c);
+
+    printf("Digite o valor a ser procurado na matriz:\n");
+    scanf("%d", &k);
+
+    searchNumber(matrix, l, c, k);
+
+
+}
+
+void searchNumber(int matrix[lin][col], int l, int c, int k){
+    int f = 0;
+
+    for(int i = 0; i < l; i++){
+        for(int j = 0; j < c; j++){
+            if(matrix[i][j] == k){
+                printf("Valor %d encontrado na posição [%d][%d]\n", k, i, j);
+                f = 1;       
+            }
+        }
+    }
+}
+
+void ordemMatrix(int *l, int *c){
+    printf("Quantidade de LINHAS:\n");
+    scanf("%d", l);
+    printf("Quantidade de COLUNAS:\n");
+    scanf("%d", c);
+}
+
+void fillMatrix(int matrix[lin][col], int *l, int *c){
+    for(int i = 0; i < *l; i++){
+        for(int j = 0; j < *c; j++){
+            printf("[%d][%d] = \n", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
